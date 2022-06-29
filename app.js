4ad1a660ebf75b241cba9f510c42b181ebf96e4b
kokayls <100805307+kokayls@users.noreply.github.com>
2022-06-29 14:43:45 +0200

    button
    function created for drawing a rounded corners button

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 
-const canvas = document.getElementById('game_canvas');
-const ctx = canvas.getContext('2d');
-
-//setup canvas size
-canvas.width = 800;
-canvas.height = 500;
-let canvasWidth = canvas.width;
-let canvasHeight = canvas.height;
-
-let bacgroundColor= "#515D5B"
-let inGame= false;
-
 let game = new Game();
 
+//test area
+    //let menuButton = new MenuButton(200, 350, 120, 40, "button", "lime");
+//test area end
 
 drawCanvas();
 
 
-function drawCanvas(){
-    window.requestAnimationFrame(drawCanvas); 
-   
-    // if(!inGame){
-    //     drawMenu();
-    // }
+
+function drawCanvas() {
     drawBackground(bacgroundColor);
-  
+    window.requestAnimationFrame(drawCanvas);
+    
+    
+   
+    //test area
+        //menuButton.render();
+    //test area end
+
+    game.update();
+
 }
 
-function drawBackground(background){
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-}
+
+
+
